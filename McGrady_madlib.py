fc8133237f4a8bf61c69c0d23c9632e548ec5daf
23mcgrli <59451378+23mcgrli@users.noreply.github.com>
2020-01-24 12:47:10 -0500

    Add files via upload

---
+++ McGrady_madlib.py
@@ -0,0 +1,42 @@
+name = input("Enter name")
+adj1 = input ("Enter adjective")
+adj2 = input ("Enter adjective")
+adv = input ("Enter adverb")
+food = input ("Enter a food")
+food2 = input ("Enter another food")
+noun = input ("Enter a noun")
+place = input ("Enter a place")
+verb = input ("Enter a verb")
+
+
+print (name,"was planing a dream vacation to",place,".")
+print (name,"was especially looking foward to trying the local cuisine, \nincluding",adj1,food,"and",food2)
+print ("\n\n"name, "will have to practice the language",adv,"to make it easier to",verb,"with people")
+print("\n\n"name,"has a long list of sights to see,including the\n"noun,"museum and the"adj2,"park.")
+
+
+
+name1 = input("enter name")
+theme = input("Enter a theme")
+place1 = input("Enter a place")
+day = input("Enter a day of the week")
+time = input("Enter time")
+verb1 = input("Enter a verb")
+animal = input("enter a animal")
+body = input("Enter a body part")
+
+print (name1,"is having a"theme,"party\n")
+print ("its going to be at"place1,"on"day"\n")
+print("please make sure to show up at"time,",or else you will be required to"verb1,"a"animal,"with your"body part,".")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ McGrady_unit1_pratice.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+print("Welcome to Information Technology\n\nprogramming and\nWeb Development")
+
+print(" _       ___________\n| |     / /_  __/   |\n| | /| / / / / / /| |\n| |/ |/ / / / / ___ |\n|__/|__/ /_/ /_/  |_|\n" )
+
+
+
+
+
+
+
+
+
+
+print ("A lightning flash:\nbetween the forest trees\nI have seen water.\n\t-shiki")
+
+
+
+
+print ("**********\n*        *\n* MR. G  *\n* RM 119 *\n*        *\n**********")
+
+
+
+
+
+
+
+print ("Computer Science is no more about computers\nthan astronomy is about telescopes\n - Edsger W. Dijkstra")
+
+
+
+
+
+
+user_name = input ("what is your name?")
+
+
+
+favorite_animal = input("what is your favorite animal?")
+
+
+
+print ("hello", user_name, ". your favorite animal is the",favorite_animal, ".")
+
+
+
+
+
+number =  int(input("what is your favorite number?"))
+
+
+
+print(number * 3)
+
+
+
+
+animal1 =  input("what is your first animal?")
+animal2 =  input("what is your second animal?")
+animal3 =  input("what is your third animal?")
+
+print (animal3, animal2, animal1)
+
+
+
+length = int(input("enter the length:"))
+Width = int(input("enter the width:"))
+
+print(" ",length,"\n\n")
+print("-------")
+print("|","   ","|")
+print("|","   ","|", Width)
+print("|","   ","|")
+print("-------")
+print("\n\narea =", length * Width)
+
+
+
+first_name = input("Please input your first name")
+last_name = input("Please input your last name")
+
+print ("first_name lastname")
+
+num = input("enter a number: ")
+print(num * 8)
+
+
+number = input("enter a number: ")
+
+print(number * 12)
+
+
+
+
+
+a = input ("enter an animal: ")
+s = input ("enter a sound")
+e = "E-I-E-I-O"
+
+print ("Old Macdonald had a farm, + e")
+print ("And on his he had a" +a+"'"+ e)
+print ("With a "+ a + "-"+ a +"here and a "+ s + "-"+ s +" there")
+print ("here a"+ s + "there a" +s)
+print ("everywhere a" + s +"-" + a)
+print ("old macdonald had a farm," + e)
